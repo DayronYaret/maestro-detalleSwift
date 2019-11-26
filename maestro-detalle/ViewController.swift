ceca31a846f8a6fe103aaaf880842b6ea7eb6c84
alumno <alumno@rdsi9.local>
2019-11-26 10:13:42 +0000

    Initial Commit

---
+++ maestro-detalle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  maestro-detalle
+//
+//  Created by alumno on 26/11/19.
+//  Copyright © 2019 Dayron. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
